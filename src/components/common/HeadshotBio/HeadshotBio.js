4a51a505cb47b194da21364959715c757201ca6d
rodneyrhkk <rho@bluon.com>
2022-11-08 14:19:29 -0800

    Fix desktop version

--- src/components/common/HeadshotBio/HeadshotBio.js
+++ src/components/common/HeadshotBio/HeadshotBio.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./_headshotBio.scss";
-import mail_icon from '../../../assets/icons/blue-mail.svg'
-import phone_icon from '../../../assets/icons/blue-phone.svg'
+import mail_icon from "../../../assets/icons/blue-mail.svg";
+import phone_icon from "../../../assets/icons/blue-phone.svg";
 const HeadshotBio = ({
   picSrc: src,
   picAlt: alt,
@@ -19,20 +19,20 @@ const HeadshotBio = ({
         <h6 className="headshot-name">{name}</h6>
         <p className="">{position}</p>
         <p className="body-small mb-0">{bio}</p>
-        {contact ?  (
+        {contact ? (
           <div className="headshot-contact">
             <a className="headshot-contact-tel" href={`tel:${tel}`}>
               {/* <img src="assets/images/icon/blue-phone.svg"/> */}
-              <img alt='phone icon' src={phone_icon}/>
-
+              <img alt="phone icon" src={phone_icon} />
             </a>
             <a className="headshot-contact-email" href={`mailto:${email}`}>
               {/* <img src="assets/images/icon/blue-mail.svg"/> */}
-              <img alt='mail icon' src={mail_icon}/>
+              <img alt="mail icon" src={mail_icon} />
             </a>
           </div>
-        ):
-        ("") }
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
